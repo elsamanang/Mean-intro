2253601df44e63d15a4d78f285d1d37f7a8e70e7
EJabulani <jabulanishany@gmail.con>
2019-12-02 16:32:37 +0200

    Correcting Post-list fiel

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import {Post} from '../post.model';
 import { PostsService } from '../posts.service';
@@ -8,13 +8,21 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit {
  // posts = [
  //   {title: 'First Post', content: 'This is the first post\'s content'},
 //  {title: 'Second Post', content: 'This is the second post\'s content'},
 //    {title: 'Third Post', content: 'This is the third post\'s content'},
 // ];
-  @Input() posts: Post [] = [];
+posts: Post [] = [];
 
-  constructor( public postsService: PostsService) {}
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) =>{
+      this.posts = posts;
+    });
+  }
 }

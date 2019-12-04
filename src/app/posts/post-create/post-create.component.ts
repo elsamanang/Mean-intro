5e81d7c3f4cf1dae94bd075f05e3e25d1e8c71d6
EJabulani <jabulanishany@gmail.con>
2019-12-04 08:23:35 +0200

    Adding resetform() method to reset the reset the form after submitting

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,8 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component} from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import {Post} from '../post.model';
+
+import { PostsService } from '../posts.service';
 
 
 
@@ -15,7 +16,9 @@ import {Post} from '../post.model';
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated  = new EventEmitter<Post>();
+
+
+  constructor(public postsService: PostsService) {}
 
 
     onAddPost( form: NgForm) {
@@ -23,10 +26,7 @@ export class PostCreateComponent {
 
         return;
       }
-      const post: Post = {
-        title: form.value.title,
-        content: form.value.content
-      };
-      this.postCreated.emit(post);
+      this.postsService.addPost(form.value.title, form.value.content);
+      form.resetForm();
   }
 }
